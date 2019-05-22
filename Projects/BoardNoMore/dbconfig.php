2ba8094a8b0dba9ac16a46f1eb83e71038ba4d92
printernick <phikhant@uci.edu>
2019-05-21 17:04:10 -0700

    Update dbconfig.php
    Hid password and database name

--- Projects/BoardNoMore/dbconfig.php
+++ Projects/BoardNoMore/dbconfig.php
@@ -1,6 +1,8 @@
 <?php
+    //replace xxxxx with password and database name
+
     define("DB_USER", "root");
-    define("DB_PASSWORD", "ii1TzDSVOPrlmbsM");
-    define("DB_DATABASE", "test");
+    define("DB_PASSWORD", "xxxxx");
+    define("DB_DATABASE", "xxxxx");
     define("DB_HOST", "localhost");
 ?>
